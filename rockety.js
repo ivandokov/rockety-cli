15144cb777bd8da7648be4c5fa6652e43a2d0767
Ivan Dokov <ivangdokov@gmail.com>
2017-03-17 15:04:12 +0200

    Fixed env for exec/execSync/spawn which fixes the proxy function for using: rockety build/watch/etc

--- rockety.js
+++ rockety.js
@@ -11,6 +11,9 @@ var copydir = require('copy-dir');
 var request = require('request');
 var unzip = require('unzip');
 var child = require('child_process');
+var exec = child.exec;
+var execSync = child.execSync;
+var spawn = child.spawn;
 var find = require('find');
 var chalk = require('chalk');
 var help = require('./help').help();
@@ -174,7 +177,7 @@ function setup(source, project, fn) {
 
     msg('Checking if yarn is installed');
     try {
-        child.execSync('yarn --version 2>/dev/null', {
+        execSync('yarn --version 2>/dev/null', {
             shell: true
         });
         packageManager = 'yarn';
@@ -193,9 +196,12 @@ function setup(source, project, fn) {
 
     msg('Running ' + packageManager + ' and bower install');
     spin.start();
-    opts = {cwd: project};
+    opts = {
+        env: process.env,
+        cwd: project
+    };
 
-    child.exec('bower install', opts, function(error, stdout, stderr) {
+    exec('bower install', opts, function(error, stdout, stderr) {
         if (error || stderr) {
             err("bower " + (error || stderr));
         }
@@ -208,7 +214,7 @@ function setup(source, project, fn) {
         complete();
     });
 
-    child.exec(packageManager + ' install --loglevel error', opts, function(error, stdout, stderr) {
+    exec(packageManager + ' install --loglevel error', opts, function(error, stdout, stderr) {
         var errMsg = (error || stderr);
         if (errMsg && errMsg.indexOf('warning') !== 0) {
             err(packageManager + " " + errMsg);
@@ -225,8 +231,9 @@ function setup(source, project, fn) {
 
 function proxy() {
     var args = Array.isArray(arguments[0]) ? arguments[0] : Array.prototype.slice.call(arguments);
-    child.spawn('gulp', args, {
+    spawn('gulp', args, {
         shell: true,
+        env: process.env,
         cwd: process.cwd(),
         stdio: "inherit"
     });
